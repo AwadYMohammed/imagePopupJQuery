39e0d876bbf549ddec27feb0f97c1732aade4a43
AwadYMohammed <awadyasirawad@gmail.com>
2020-09-08 02:06:11 -0400

    set overlay properties

--- jquery.image-popup.js
+++ jquery.image-popup.js
@@ -1,6 +1,25 @@
 (function ($){
 	$.fn.imagePopup = function(){
+		/*
+			*iterating through each image Gallery
+			*
+			*
+			*
+			*/
 		return this.each(function(){
+			/*
+			*declaring new elements variable
+			*
+			*/
+			var $overlay;
+			setOverlayProperties();
+			function setOverlayProperties(){
+				$overlay = $('<di></div>');
+				$overlay.css({"background": 'rgba(0, 0, 0, 0.5)', "position": "absolute", "top":"0px", "left":"0px", "display": "none", "text-align":"center", "width": "100%", "height": "100%", "padding-top": "05%");
+				
+			}
+			
+			
 		})
 	}
 }(jQuery));
