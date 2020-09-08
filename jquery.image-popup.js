3862a684717bab54ab19236aee8ad81b8dbcf125
AwadYMohammed <awadyasirawad@gmail.com>
2020-09-08 02:11:06 -0400

    Adding some functionality to js file

--- jquery.image-popup.js
+++ jquery.image-popup.js
@@ -13,6 +13,13 @@
 			*/
 			var $overlay;
 			setOverlayProperties();
+			$(this).find("a").on("click", function(event){
+				event.preventDefault();
+				$overlay.show();
+				
+			})
+			
+			
 			function setOverlayProperties(){
 				$overlay = $('<di></div>');
 				$overlay.css({"background": 'rgba(0, 0, 0, 0.5)', "position": "absolute", "top":"0px", "left":"0px", "display": "none", "text-align":"center", "width": "100%", "height": "100%", "padding-top": "05%");
